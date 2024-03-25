e01ef37003af5123172f14e4e8eb0b76f4cb4960
Vickouma77 <Vickprestone20@gmail.com>
2024-03-25 09:08:47 +0300

    number guessing game

---
+++ 0x00number_guess.py
@@ -0,0 +1,17 @@
+import random
+
+def number_guess():
+
+    print("Welcome to the number guessing game!\n")
+
+    number = random.randrange(1, 100)
+    guess = 0
+    while guess != number:
+        guess = int(input("Enter a number between 1 and 100: "))
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+    print("You got it!")
+
+number_guess()
