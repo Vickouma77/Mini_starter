937f71e572864e9b8706c6d65101fdeb846ef237
Vickouma77 <Vickprestone20@gmail.com>
2024-03-26 12:56:34 +0300

    Implement findMissingNumbers function to identify missing integers in a sequence

---
+++ missingNumbers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def findMissingNumbers(num_list):
+    numbers = set(num_list)
+    missing = []
+
+    for i in range(1, num_list[-1]):
+        if i not in numbers:
+            missing.append(i)
+    return missing
+
+""" list_n = [1, 2, 4, 6, 7, 9]
+
+print(findMissingNumbers(list_n)) """  # [3, 5, 8]
