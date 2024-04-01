0edb65b420b3f749c03f4b842b757935e05f6105
Vickouma77 <Vickprestone20@gmail.com>
2024-04-01 14:41:12 +0300

    . Simple delete by key

---
+++ python_data_structures/8-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+simple_delete = __import__('8-simple_delete').simple_delete
+
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+new_dict = simple_delete(a_dictionary, 'track')
+print(new_dict)
+
+print("--")
