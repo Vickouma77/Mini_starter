85aaf6dbe9b2d89535320f0d755a21520f8fa0dc
Vickouma77 <Vickprestone20@gmail.com>
2024-04-18 14:28:39 +0300

    grade checker

---
+++ rust/grades_checker/target/debug/deps/grades_checker-ef6274df89d4f8bd.d
@@ -0,0 +1,5 @@
+/home/vick/Mini_starter/rust/grades_checker/target/debug/deps/grades_checker-ef6274df89d4f8bd: src/main.rs
+
+/home/vick/Mini_starter/rust/grades_checker/target/debug/deps/grades_checker-ef6274df89d4f8bd.d: src/main.rs
+
+src/main.rs:
---
+++ rust/grades_checker/target/debug/grades_checker.d
@@ -0,0 +1,1 @@
+/home/vick/Mini_starter/rust/grades_checker/target/debug/grades_checker: /home/vick/Mini_starter/rust/grades_checker/src/main.rs
