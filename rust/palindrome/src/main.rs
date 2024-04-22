0fa486faf3363dbe5540e1280b56fea5d1b36777
Vickouma77 <Vickprestone20@gmail.com>
2024-04-22 14:57:57 +0300

    palindrome

---
+++ rust/palindrome/src/main.rs
@@ -0,0 +1,13 @@
+
+fn is_palindrome(s: &str) -> bool {
+    let s = s.to_lowercase();
+    let s = s.chars().filter(|c| c.is_alphanumeric()).collect::<String>();
+    let reversed = s.chars().rev().collect::<String>();
+    s == reversed
+}
+
+fn main() {
+    let input = "A man, a plan, a canal, Panama!";
+    println!("Is \"{}\" a palindrome? {}", input, is_palindrome(input));
+}
+
