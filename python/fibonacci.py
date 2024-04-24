57b3b7c4b40dc5e4ec55f234c4aae4f3247b1a14
Vickouma77 <Vickprestone20@gmail.com>
2024-04-24 18:38:24 +0300

    update

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -2,9 +2,10 @@
 
 def fib(n):
     """Print a Fibonacci series up to n."""
-    a, b = 0, 1
-    while a < n:
-        print(a, end=' ')
-        a, b = b, a+b
+    current_fib, next_fib = 0, 1
+    for _ in range(0, n):
+        fib_number = current_fib
+        current_fib, next_fib = next_fib, current_fib + next_fib
+        print(fib_number, end=' ')
 
 fib(20)
