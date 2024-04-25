63955261a5c38768b3416dbb2b0296ac675d4dcc
Vickouma77 <Vickprestone20@gmail.com>
2024-04-25 15:40:23 +0300

    common in both

---
+++ python/python_data_structures/15-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def common_elements(set_1, set_2):
+    """
+    function that returns a set of common elements in two sets
+    """
+    common = set_1 & set_2
+
+    return common
